8f89d934e28e7ec81e7d43b71702f3b8a3fe50f9
102464 <pxesrv@outlook.com>
2026-07-12 11:03:08 +0000

    stone: disable deprecated HIDL CAS HAL service
    Set TARGET_REQUIRES_HIDL_CAS_HAL := false before inheriting base products.
    Device uses AIDL CAS HAL (com.android.hardware.cas APEX) instead of
    deprecated HIDL CAS 1.2 service.
    
    Change-Id: Ie2e60b31c5681057f938fb9f1d28d850c9c112a1

--- lineage_stone.mk
+++ lineage_stone.mk
@@ -3,6 +3,11 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Device uses AIDL CAS HAL (com.android.hardware.cas APEX) instead of
+# deprecated HIDL CAS 1.2 service. Set this before inheriting base products,
+# where build/target/product/base_vendor.mk decides default vendor packages.
+TARGET_REQUIRES_HIDL_CAS_HAL := false
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 TARGET_SUPPORTS_OMX_SERVICE := false
