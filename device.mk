d00d4c5fd28341e1cba17fabb5a82d2990e08be3
Rimaki <rimaki5891@gmail.com>
2026-09-01 20:15:20 +0700

    stone: Add support bypass charge
    Change-Id: I4886c1090dde7c49c49c60b0ea9039daeb28c80d

--- device.mk
+++ device.mk
@@ -194,7 +194,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
-$(call soong_config_set_bool,lineage_health,charging_control_supports_bypass,false)
+$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/battery_charging_enabled)
+$(call soong_config_set,lineage_health,charging_control_charging_enabled,1)
+$(call soong_config_set,lineage_health,charging_control_charging_disabled,0)
+$(call soong_config_set_bool,lineage_health,charging_control_supports_bypass,true)
 
 # HIDL
 PRODUCT_PACKAGES += \
